7f0c3c0221d08008008d6e3a32499f58aadcafa4
lucasnather <natherlucas@gmail.com>
2024-11-22 15:44:38 -0400

    chore: cors policy

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import swaggerUi from 'swagger-ui-express'
 import { env } from './env'
 import { rideRouter } from './infra/routes/ride-router'
 import { openapiSpecification } from './swagger/options'
+import cors from "cors"
 
 const app = express()
 
 const port = env.PORT
 app.use(express.json())
-
+app.use(cors({
+    methods: ["POST", "PATCH", "GET"],
+    origin: "htto://localhost"
+}))
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
 
 app.get("/test", async (req, res) => {
